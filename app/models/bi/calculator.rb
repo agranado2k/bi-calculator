e5143ba2ac8999d60ae205193a24773793f7af4f
Arthur Granado <agranado2k@gmail.com>
2018-09-09 22:13:31 +0100

    Include some comment on Calculator

--- app/models/bi/calculator.rb
+++ app/models/bi/calculator.rb
@@ -21,18 +21,29 @@ module BI
     def polynomio_to_s(pol)
       r = ''
       pol.each_with_index do |n, i|
+        # if n is zero the sub polynomio should not be showed
         next if n.zero?
+
+        # include the operator in front the new sub polynomio
+        # if it's not the first one
         r += (n > 0 ? ' + ' : ' - ') if !i.zero?
+
+        # if the sub polynomio is negaive invert the sign
         n *= -1 if n < 0
+
+        # calculate the expoente
         exp = pol.size - i - 1
+
+        # print only n if expoente is zero
         if exp.zero?
           r += n.to_s
+
+        # print the only 'x' if the expoent is one
         elsif exp == 1
           r += "#{n}x"
         else
           r += "#{n}x^#{exp}"
         end
-
       end
       r
     end
